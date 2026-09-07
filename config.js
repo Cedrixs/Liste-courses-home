55b1dc9bdef3e26c62bf51f74485340b502c5f32
Cedrixs <52997512+Cedrixs@users.noreply.github.com>
2026-09-07 15:00:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Collez ici l'URL de votre déploiement Google Apps Script (se termine par /exec).
 // Voir README.md pour la procédure complète.
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/REMPLACER_PAR_VOTRE_ID/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzvSFTsOOwoK19ocb4EGPXQ_-dIidveogK5uA2fP1Hdxk1rNCE6IPBVQPsQZsnyGBAkkQ/exec',
   CATEGORIES: [
     'Fruits & Légumes',
     'Crèmerie',
